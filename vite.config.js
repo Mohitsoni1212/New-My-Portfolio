26d0e684daf4cf5235a493922f3289d5ef443bb2
Mohit soni <mohitsoni2241@gmail.com>
2026-02-08 14:30:54 +0530

    Build ready for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/New-My-Portfolio/', // ✅ GitHub Pages or Netlify repo folder ke liye
 })
