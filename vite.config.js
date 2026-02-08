6a3bb580553e0087519016c1279f0608bbe4ee38
Mohit soni <mohitsoni2241@gmail.com>
2026-02-08 14:34:53 +0530

    Prepare for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New-My-Portfolio/', // ✅ GitHub Pages or Netlify repo folder ke liye
+  base: '/New-My-Portfolio/',   // ✅ GitHub repo ka exact name
 })
